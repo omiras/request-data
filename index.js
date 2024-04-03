5f317f39dad8e655822ff9fd955efedbdc10cada
omiras <oscar.miras@gmail.com>
2024-04-03 15:25:54 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+app.use(cors());
+
+// POST route
+app.post('/register', (req, res) => {
+    // Log received data
+    console.log('Received data:', req.body);
+
+    // Send response back to the user
+    res.json({
+        message: 'Datos de inscripción recibidos:',
+        data: req.body
+    });
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
