37a4cbfb7afb4e94211d68d5405af8a0d5a9c542
omiras <oscar.miras@gmail.com>
2024-04-03 15:37:13 +0200

    done

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 // POST route
